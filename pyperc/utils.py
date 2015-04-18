9cd3f08eb5a94ae84877430af04d0214c4e01109
Benjamin Yates <benjamin@rqdq.com>
2015-04-18 11:17:00 -0400

    split() can do this

--- pyperc/utils.py
+++ pyperc/utils.py
@@ -4,10 +4,8 @@ import re
 from datetime import datetime
 
 def megasplit(line):
-    mid = line.index(':')
-    left = line[0:mid].strip()
-    right = line[mid+1:].strip()
-    return left, right
+    left, right = line.split(':', 1)
+    return left.strip(), right.strip()
 
 MEGADATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 def from_megadate(val):
