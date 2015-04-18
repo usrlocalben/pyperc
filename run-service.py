13be2b10bd702fd61f84c3c22f7f3d5e5bd317b4
Benjamin Yates <benjamin@rqdq.com>
2015-04-18 11:57:41 -0400

    drop django

--- run-service.py
+++ run-service.py
@@ -7,9 +7,11 @@ import pprint
 import urlparse
 from itertools import takewhile
 
-import twisted.internet.reactor
 import twisted.web.server
-import twisted.web.resource
+
+from twisted.internet import reactor
+from twisted.web.resource import Resource
+from twisted.web.static import File
 
 from pyperc import PyPerc, MegaCLI, MegaCLIRunner
 
@@ -24,9 +26,9 @@ pa = PyPerc(
 def percpoll(s):
     pa.poll()
     last_event = pa.last_event()
-    twisted.internet.reactor.callLater(PERC_POLL_PERIOD, percpoll, "again")
+    reactor.callLater(PERC_POLL_PERIOD, percpoll, "again")
 
-twisted.internet.reactor.callLater(0, percpoll, "first")
+reactor.callLater(0, percpoll, "first")
 
 
 def limited_to(limit, iterable):
@@ -37,59 +39,88 @@ def limited_to(limit, iterable):
         yield item
         total += 1
 
-class percapi(twisted.web.resource.Resource):
+
+class PercApiInfo(Resource):
     isLeaf = True
     request_count = 0
 
     def render_GET(self, request):
 
         remote_host = request.getHost() #( twisted.internet.address.IPv4Address .host, .port, .type )
-        print "%s %s" % (remote_host.host, request.uri)
-
-        #up = urlparse.urlparse(request.uri)
-        #qd = urlparse.parse_qs(up.query)
-        args = {k:v[0] for k,v in urlparse.parse_qs(urlparse.urlparse(request.uri).query).iteritems()}
+        print "adapter %s %s" % (remote_host.host, request.uri)
 
         self.request_count += 1
         request.setHeader("Content-type", "application/json")
 
-        if request.path == '/info/':
-            return ujson.dumps({
-                'success': True,
-                'ad': pa.adapter_details,
-                'cf': pa.config_details,
-                'ld': pa.ldinfo,
-                'pd': pa.pdinfo,
-                'pd_to_ld': pa.pd_to_ld,
+        return ujson.dumps({
+            'success': True,
+            'ad': pa.adapter_details,
+            'cf': pa.config_details,
+            'ld': pa.ldinfo,
+            'pd': pa.pdinfo,
+            'pd_to_ld': pa.pd_to_ld,
+        })
+
+
+class PercApiEvents(Resource):
+    isLeaf = True
+    request_count = 0
+
+    def render_GET(self, request):
+        remote_host = request.getHost() #( twisted.internet.address.IPv4Address .host, .port, .type )
+        print "events - %s %s" % (remote_host.host, request.uri)
+
+        self.request_count += 1
+        request.setHeader("Content-type", "application/json")
+
+        try:
+            since = request.args['since'][0]
+        except:
+            since = None
+
+        try:
+            limit = request.args['limit'][0]
+        except:
+            limit = None
+
+        ignore = (30, 113, 236)
+
+        if not since:
+            events = reversed(pa.events.data)
+            events = (item for item in events if item.code not in (30, 113, 236))
+            if limit is not None:
+                events = limited_to(limit, events)
+            events = (item.to_dict() for item in events)
+        else:
+            events = pa.events.find_gt(int(since), limit=limit, filter=lambda event: event.id not in (30, 113, 236))
+            events = (item.to_dict() for item in events)
+
+        events = (item for item in events if item['code'] not in ignore)
+        events = sorted(events, key=lambda item: item['id'])
+        return ujson.dumps({
+            'success': True,
+            'events': events,
             })
 
-        elif request.path == '/events/':
-            since = args.get('since', None)
-            limit = args.get('limit', None)
+static = Resource()
+static.putChild("jquery", File("bower_components/jquery"))
+static.putChild("moment", File("bower_components/moment"))
+static.putChild("knockout", File("bower_components/knockout"))
+static.putChild("bootstrap", File("bower_components/bootstrap"))
+static.putChild("bootswatch", File("bower_components/bootswatch"))
+static.putChild("fontawesome", File("bower_components/fontawesome"))
+static.putChild("dashboard.js", File("static/dashboard.js"))
+static.putChild("require.js", File("static/require.js"))
 
-            if not since:
-                events = reversed(pa.events.data)
-                events = (item for item in events if item.code not in (30, 113, 236))
-                if limit is not None:
-                    events = limited_to(limit, events)
-                serialized = (item.to_dict() for item in events)
-                return ujson.dumps({
-                    'success': True,
-                    'events': list(serialized),
-                    })
-            else:
-                events = pa.events.find_gt(int(since), limit=limit, filter=lambda event: event.id not in (30, 113, 236))
-                serialized = (item.to_dict() for item in events)
-                return ujson.dumps({
-                    'success': True,
-                    'events': list(serialized),
-                })
+api = Resource()
+api.putChild("events", PercApiEvents())
+api.putChild("adapter", PercApiInfo())
 
-        else:
-            print 'bad request: ['+request.path+']'
-            return ujson.dumps({'success':False,'message':'unknown request'})
+root = Resource()
+root.putChild("api", api)
+root.putChild("static", static)
+root.putChild("home", File("static/dashboard.html"))
 
-
-twisted.internet.reactor.listenTCP(50001, twisted.web.server.Site(percapi()))
-twisted.internet.reactor.run()
+reactor.listenTCP(50001, twisted.web.server.Site(root))
+reactor.run()
 
